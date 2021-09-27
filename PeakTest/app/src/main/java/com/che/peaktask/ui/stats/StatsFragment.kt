9c9f72ec3f18432f35cd89bbf634d7e8ce2dbd43
Yevhen Chernets <yevhen.chernets@wdc.com>
2021-09-27 23:39:21 +0300

    Stats Screen implemented

--- PeakTest/app/src/main/java/com/che/peaktask/ui/stats/StatsFragment.kt
+++ PeakTest/app/src/main/java/com/che/peaktask/ui/stats/StatsFragment.kt
@@ -6,13 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.che.peaktask.databinding.FragmentStatsBinding
+import com.che.peaktask.ui.home.MainViewModel
+import com.che.peaktask.usecase.PeakShape
 
 class StatsFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private val mainViewModel: MainViewModel by activityViewModels()
     private var _binding: FragmentStatsBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,16 +27,21 @@ class StatsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentStatsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        val textViewSquares: TextView = binding.textSquares
+        val textViewCircles: TextView = binding.textCircles
+        val textViewTriangles: TextView = binding.textTriangles
+        mainViewModel.shapesData.observe(viewLifecycleOwner, {
+            textViewSquares.text = it.filter { it.shapeType == PeakShape.Type.Square }.size.toString()
+            textViewCircles.text = it.filter { it.shapeType == PeakShape.Type.Circle }.size.toString()
+            textViewTriangles.text = it.filter { it.shapeType == PeakShape.Type.Triangle }.size.toString()
         })
+
+        binding.viewModel = mainViewModel
+        binding.lifecycleOwner = this
         return root
     }
 
